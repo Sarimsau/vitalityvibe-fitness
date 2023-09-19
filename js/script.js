6178cf365032d7de1daf2539b9f46191d7ae9426
Sarimsau <134043594+Sarimsau@users.noreply.github.com>
2023-09-19 14:33:18 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// initializing the variables 
+// let counterNumber = document.querySelectorAll('.num-item');
+// let speed = 50;
+
+
+// counterNumber.forEach((element)=>{
+
+//     let updateNumber = ()=>{
+//         let targetNumber = parseInt(element.dataset.number);
+//         // console.log(targetNumber)
+
+//         //getting the initial number of num-item element
+//         let initialNum = parseInt(element.innerText)
+//         // console.log(initialNum)
+
+//         let incrementNum = Math.trunc(targetNumber / speed)
+//         // console.log(incrementNum)
+//         if (initialNum<targetNumber) {
+//             element.innerText =`${initialNum + incrementNum } +`
+//             setTimeout(updateNumber, 10)
+//         }
+//     }
+
+//     updateNumber();
+// })
